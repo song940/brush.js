ae55df4161a78336a4a3d71b20e9eda4437ae30b
Lsong <song940@gmail.com>
2016-10-22 23:00:12 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Brush.js
+
+> A canvas library focused on mobile applications.
+
+### Example
+
+```js
+var el = document.getElementById('#canvas');
+
+var brush = new Brush(el);
+
+brush.draw();
+```
---
+++ index.js
@@ -0,0 +1,18 @@
+
+/**
+ * [Brush description]
+ * @param {[type]} ctx [description]
+ */
+function Brush(ctx){
+  this.ctx = ctx;
+};
+
+/**
+ * [draw description]
+ * @return {[type]} [description]
+ */
+Brush.prototype.draw = function(){
+  // body...
+};
+
+module.exports = Brush;
