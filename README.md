7da046d3486ae6019e44108c13f7e4ebb7822059
Lsong <song940@gmail.com>
2016-10-27 23:00:03 +0800

    add new apis

--- README.md
+++ README.md
@@ -5,15 +5,14 @@
 ### Example
 
 ```js
-var el = document.getElementById('#canvas');
+var el = document.getElementById('canvas');
+  var brush = new Brush(el);
 
-var brush = new Brush(el);
-
-brush
-.background()
-.arc()
-.rect()
-.draw()
-.save()
-
+  brush
+  .background('#f70')
+  .text('test', 50, 50)
+  .arc(50, 50)
+  .rect(30, 30, 100, 100)
+  .line(50, 50, 100, 100)
+  .image('https://mdn.mozillademos.org/files/5397/rhino.jpg')
 ```
---
+++ brush.js
@@ -0,0 +1,157 @@
+
+/**
+ * [Brush description]
+ * @param {[type]} ctx [description]
+ */
+function Brush(ctx){
+  if(!(this instanceof Brush))
+    return new Brush(ctx);
+  if(ctx instanceof HTMLElement)
+    ctx = canvas.getContext("2d");
+
+  this.ctx = ctx;
+  this.canvas = this.ctx.canvas;
+  this.width  = this.ctx.canvas.width;
+  this.height = this.ctx.canvas.height;
+  return this;
+};
+
+Brush.prototype.text = function(text, x, y, font, color){
+  x = x || 0;
+  y = y || 0;
+  font = font || '48px serif';
+  this.ctx.font = font;
+  this.ctx.fillStyle = color || 'black';
+  this.ctx.fillText(text, x, y);
+  return this;
+};
+
+/**
+ * [image description]
+ * @param  {[type]} src     [description]
+ * @param  {[type]} dx      [description]
+ * @param  {[type]} dy      [description]
+ * @param  {[type]} dWidth  [description]
+ * @param  {[type]} dHeight [description]
+ * @param  {[type]} sx      [description]
+ * @param  {[type]} sy      [description]
+ * @param  {[type]} sWidth  [description]
+ * @param  {[type]} sHeight [description]
+ * @return {[type]}         [description]
+ */
+Brush.prototype.image = function(src, dx, dy, dWidth, dHeight, sx, sy, sWidth, sHeight){
+  dx = dx || 0;
+  dy = dy || 0;
+  dWidth  = dWidth  || this.width;
+  dHeight = dHeight || this.height;
+  sx = sx || 0;
+  sy = sy || 0;
+  var img = new Image();
+  img.src = src;
+  this.ctx.drawImage(img, dx, dy, dWidth, dHeight, sx, sy, sWidth, sHeight);
+};
+/**
+ * [rotate description]
+ * @param  {[type]} deg [description]
+ * @return {[type]}     [description]
+ */
+Brush.prototype.rotate = function(deg){
+  this.ctx.rotate(deg * Math.PI / 180);
+  return this;
+};
+/**
+ * [line description]
+ * @param  {[type]} x  [description]
+ * @param  {[type]} y  [description]
+ * @param  {[type]} x1 [description]
+ * @param  {[type]} y1 [description]
+ * @return {[type]}    [description]
+ */
+Brush.prototype.line = function(x, y, x1, y1){
+  x = x || 0;
+  y = y || 0;
+  x1 = x1 || 0;
+  y1 = y1 || 0;
+  this.ctx.beginPath();
+  this.ctx.moveTo(x, y);
+  this.ctx.lineTo(x1, y1);
+  this.ctx.stroke();
+  return this;
+};
+/**
+ * [rect description]
+ * @param  {[type]} x      [description]
+ * @param  {[type]} y      [description]
+ * @param  {[type]} width  [description]
+ * @param  {[type]} height [description]
+ * @param  {[type]} color  [description]
+ * @return {[type]}        [description]
+ */
+Brush.prototype.rect = function(x, y, width, height){
+  x = x || 0;
+  y = y || 0;
+  width  = width  || this.width;
+  height = height || this.height;
+  this.ctx.rect(x, y, width, height);
+  this.ctx.stroke();
+  return this;
+};
+/**
+ * [arc description]
+ * @param  {[type]} x             [description]
+ * @param  {[type]} y             [description]
+ * @param  {[type]} radius        [description]
+ * @param  {[type]} startAngle    [description]
+ * @param  {[type]} endAngle      [description]
+ * @param  {[type]} anticlockwise [description]
+ * @return {[type]}               [description]
+ */
+Brush.prototype.arc = function(x, y, radius, startAngle, endAngle, anticlockwise){
+  x = x || 0;
+  y = y || 0;
+  radius = radius || 50;
+  startAngle = startAngle || 0;
+  endAngle   = endAngle   || Math.PI * 2;
+  this.ctx.beginPath();
+  this.ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+  this.ctx.stroke();
+  return this;
+};
+/**
+ * [background description]
+ * @param  {[type]} color [description]
+ * @return {[type]}       [description]
+ */
+Brush.prototype.background = function(color){    
+  return this.fill(null, null, null, null, color);
+};
+/**
+ * [fill description]
+ * @param  {[type]} color  [description]
+ * @param  {[type]} x      [description]
+ * @param  {[type]} y      [description]
+ * @param  {[type]} width  [description]
+ * @param  {[type]} height [description]
+ * @return {[type]}        [description]
+ */
+Brush.prototype.fill = function(x, y, width, height, color){
+  this.ctx.fillStyle = color || 'white';
+  this.ctx.fillRect(x, y, width, height);
+  return this.rect(x, y, width, height, color);
+};
+/**
+ * [clear description]
+ * @param  {[type]} x      [description]
+ * @param  {[type]} y      [description]
+ * @param  {[type]} width  [description]
+ * @param  {[type]} height [description]
+ * @return {[type]}        [description]
+ */
+Brush.prototype.clear = function(x, y, width, height){
+  x = x || 0;
+  y = y || 0;
+  width  = width  || this.width;
+  height = height || this.height;
+  this.ctx.clearRect(x, y, width, height);
+  return this;
+};
